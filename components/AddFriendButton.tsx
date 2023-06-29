d284b54652e1c9a77c706501781385fae8cf8358
Michael Hofer <hoferdev.mi@gmail.com>
2023-06-29 16:51:54 +0200

    change api adress to correct name

--- components/AddFriendButton.tsx
+++ components/AddFriendButton.tsx
@@ -6,7 +6,7 @@ type AddFriendButtonProps = {
 export default function AddFriendButton({ friendId }: AddFriendButtonProps) {
   const handleAddFriend = async () => {
     try {
-      const response = await fetch('/api/addfriend', {
+      const response = await fetch('/api/addfriendbyid', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -42,7 +42,7 @@ export default async function Header() {
             </Link>
             <Link
               className="lock mt-4 lg:inline-block lg:mt-0 text-secondary hover:text-secondary-foreground mr-4"
-              href="/login"
+              href="/friends"
             >
               Friends
             </Link>
