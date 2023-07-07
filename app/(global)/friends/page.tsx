fd0c905cd187eeb0538abe870d026b058a1b7bc2
Michael Hofer <hoferdev.mi@gmail.com>
2023-07-07 12:22:44 +0200

    add auth protection to userlist and friendlist

--- app/(global)/friends/page.tsx
+++ app/(global)/friends/page.tsx
@@ -1,9 +1,28 @@
+import { cookies } from 'next/headers';
+import { getValidSessionByToken } from '../../../database/sessions';
 import FriendListAndRequest from './components/FriendListAndRequest';
 
-export default function FriendsPage() {
+export default async function FriendsPage() {
+  const sessionTokenCookie = cookies().get('sessionToken');
+  // 2. check if the sessionToken has a valid session
+  const session =
+    sessionTokenCookie &&
+    (await getValidSessionByToken(sessionTokenCookie.value));
   return (
-    <div>
-      <FriendListAndRequest />
-    </div>
+    <main>
+      {!session ? (
+        <section className="bg-primary-background dark:bg-gray-900">
+          <div className="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1>You need to login first!</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      ) : (
+        <FriendListAndRequest />
+      )}
+    </main>
   );
 }
--- app/(global)/users/page.tsx
+++ app/(global)/users/page.tsx
@@ -1,9 +1,28 @@
+import { cookies } from 'next/headers';
+import { getValidSessionByToken } from '../../../database/sessions';
 import UserList from './UserList';
 
-export default function usersPage() {
+export default async function usersPage() {
+  const sessionTokenCookie = cookies().get('sessionToken');
+  // 2. check if the sessionToken has a valid session
+  const session =
+    sessionTokenCookie &&
+    (await getValidSessionByToken(sessionTokenCookie.value));
   return (
     <main>
-      <UserList />
+      {!session ? (
+        <section className="bg-primary-background dark:bg-gray-900">
+          <div className="flex flex-col items-center justify-center px-6 mx-auto md:h-screen lg:py-0">
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1>You need to login first!</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      ) : (
+        <UserList />
+      )}
     </main>
   );
 }
