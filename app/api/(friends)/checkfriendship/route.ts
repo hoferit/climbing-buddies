65529479644362dbe85959e49b64486d1e6f3275
Michael Hofer <hoferdev.mi@gmail.com>
2023-07-10 17:31:07 +0200

    add checkfriendship api route

---
+++ app/api/(friends)/checkfriendship/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { checkFriendship } from '../../../../database/friends';
+import { authorizeAndAuthenticate } from '../../../../utils/auth';
+
+export async function GET(request: NextRequest): Promise<NextResponse<any>> {
+  try {
+    // Check authorization and authentication
+    await authorizeAndAuthenticate();
+
+    // Parse the request body
+    const { userId, friendId } = await request.json();
+
+    // Check the friendship
+    const isFriend = await checkFriendship(userId, friendId);
+
+    // Return the result
+    return NextResponse.json({ isFriend }, { status: 200 });
+  } catch (error) {
+    const errorMessage = (error as Error).message;
+
+    return NextResponse.json({ error: errorMessage }, { status: 401 });
+  }
+}
