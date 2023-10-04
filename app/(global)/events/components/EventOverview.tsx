7e86351f6ad6da4f45c40616c1cbb0213857908b
Michael Hofer <hoferdev.mi@gmail.com>
2023-10-04 16:09:21 +0200

    Add create event link on EventOverview component

--- app/(global)/events/components/EventOverview.tsx
+++ app/(global)/events/components/EventOverview.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link';
+
 export default function EventOverview() {
   return (
     <>
       <section>
         <h1>Events</h1>
+        <Link href="/create-event">Create Event</Link>
       </section>
       <section>
         <h2>Search Bar and filters</h2>
