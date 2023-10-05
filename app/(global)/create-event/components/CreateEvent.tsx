c843b80bbc094c36e39b77695204609dbced7fc2
Michael Hofer <hoferdev.mi@gmail.com>
2023-10-05 18:24:54 +0200

    add zod event schema

--- app/(global)/create-event/components/CreateEvent.tsx
+++ app/(global)/create-event/components/CreateEvent.tsx
@@ -1,3 +1,19 @@
+import * as z from 'zod';
+
+const createEventSchema = z.object({
+  title: z.string().min(1, 'Title is required').max(100),
+  description: z.string().min(1, 'Description is required'),
+  organiserId: z
+    .number()
+    .int()
+    .positive('Organizer ID should be a positive integer'),
+  styleId: z.number().int().positive('Style ID should be a positive integer'),
+  startTime: z.string().min(1, 'Start time is required'), // TODO: implement date-fns library
+  endTime: z.string().min(1, 'End time is required'),
+  gymId: z.number().int().positive('Gym ID should be a positive integer'),
+  status: z.enum(['UPCOMING', 'PAST', 'CANCELLED']), // TODO: create EventStatus enum in Prisma Schema
+});
+
 export default function CreateEvent() {
   return (
     <div>
